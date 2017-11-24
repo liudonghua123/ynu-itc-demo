6f6b0b7ee75243aa7c01cd9a907edbbc90ccb5a2
liudonghua123 <liudonghua123@gmail.com>
2017-11-24 11:56:08 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+var bowerFiles = require('main-bower-files');
+var es = require('event-stream');
+var gls = require('gulp-live-server');
+var watch = require('gulp-watch');
+var del = require('del');
+
+gulp.task('clean', function () {
+  return del(['dist']);
+});
+
+// Copy all static bower files
+gulp.task('bower', function () {
+  return gulp.src(bowerFiles(), { base: 'bower_components' })
+    .pipe(gulp.dest('./src/vendor/'));
+});
+
+gulp.task('inject', ['bower'], function () {
+  return gulp.src('./src/index.html')
+  .pipe(inject(gulp.src(['./src/vendor/**/*.js', './src/vendor/**/*.css'], {read: false}), {name: 'bower', relative: true}))
+  .pipe(inject(gulp.src(['./src/**/*.js', './src/**/*.css', '!./src/vendor/**/*.js', '!./src/vendor/**/*.css'], {read: false}), {relative: true}))
+  .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch', function () {
+  return gulp.watch('./src/index.html', function (file) {
+      gulp.start('inject');
+    });
+});
+
+gulp.task('resources', function () {
+    return gulp.watch(['./src/**/*'], function (file) {
+      gulp.src(['./src/**/*', '!./src/index.html'], {base: 'src'})
+            .pipe(gulp.dest('./dist/'));
+      });
+});  
+
+gulp.task('serve', ['resources'], function() {
+    var server = gls.static(['dist'], 8000);
+    server.start();
+    //use gulp.watch to trigger server actions(notify, start or stop)
+    return gulp.watch(['./dist/**/*'], function (file) {
+      server.notify.apply(server, [file]);
+    });
+  });
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['bower', 'inject', 'watch', 'serve']);
---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+angular.module('itcDataApp', [])
+  .controller('itcDataController', function($scope, $http) {
+    $scope.categories = [];
+    $scope.firstKeyName = function (obj) {
+        return Object.keys(obj)[0];
+    };
+    $scope.firstKeyValues = function (obj) {
+        return obj[Object.keys(obj)[0]];
+    };
+    $scope.randomIcon = function() {
+        var availableIcons = ["mif-steps","mif-squirrel","mif-ambulance","mif-justice","mif-file-excel","mif-cab","mif-bicycle","mif-train"];
+        return availableIcons[parseInt(Math.random() * (availableIcons.length))];
+    };
+    $scope.randomBgColor = function() {
+        var availableColors = ["bg-indigo","bg-cyan","bg-amber","bg-darkCyan","bg-pink","bg-violet","bg-brown","bg-grayDark","bg-cobalt"];
+        return availableColors[parseInt(Math.random() * (availableColors.length))];
+    };
+    $http.get('data.json').then(function(data,status,headers,congfig){
+            console.info(data);
+            $scope.categories = data.data.categories;
+        }, function(data,status,headers,congfig){
+            console.error(data);
+            $scope.categories = [];
+        });
+  });

289506fcb96301371c0f761169261dd3bde58c39
liudonghua123 <liudonghua123@gmail.com>
2017-11-27 09:40:10 +0800

    change $scope to this

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,30 @@
 angular.module('itcDataApp', [])
   .controller('itcDataController', function($scope, $http) {
-    $scope.categories = [];
-    $scope.firstKeyName = function (obj) {
+    var itcData = this;
+    itcData.categories = [];
+    itcData.firstKeyName = function (obj) {
         return Object.keys(obj)[0];
     };
-    $scope.firstKeyValues = function (obj) {
+    itcData.firstKeyValues = function (obj) {
         return obj[Object.keys(obj)[0]];
     };
-    $scope.randomIcon = function() {
+    itcData.randomIcon = function() {
         var availableIcons = ["mif-steps","mif-squirrel","mif-ambulance","mif-justice","mif-file-excel","mif-cab","mif-bicycle","mif-train"];
         return availableIcons[parseInt(Math.random() * (availableIcons.length))];
     };
-    $scope.randomBgColor = function() {
+    itcData.randomBgColor = function() {
         var availableColors = ["bg-indigo","bg-cyan","bg-amber","bg-darkCyan","bg-pink","bg-violet","bg-brown","bg-grayDark","bg-cobalt"];
         return availableColors[parseInt(Math.random() * (availableColors.length))];
     };
-    $scope.yaml = function() {
+    itcData.yaml = function() {
         return window.yaml;
     }
 
     $http.get('data.yml').then(function(data,status,headers,congfig){
             console.info(data);
-            $scope.categories = window.jsyaml.load(data.data).categories;
+            itcData.categories = window.jsyaml.load(data.data).categories;
         }, function(data,status,headers,congfig){
             console.error(data);
-            $scope.categories = [];
+            itcData.categories = [];
         });
   });
